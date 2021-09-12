39951fdd3cf7c5f8728eace27449d33a85aaef52
Hoang Thuan <hoangthuan171098@gmail.com>
2021-09-13 00:01:24 +0700

    add order

---
+++ src/view/public/home.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import {Hero,Ctasection,Services,Portfolio,Client} from './components/export'
+
+class Home extends Component{
+    render(){
+        return (
+            <div>
+                <button className='btn btn-success'></button>
+                <section id="hero">
+                    <Hero/>
+                </section>
+
+                <section id="cta" className="cta">
+                    <Ctasection/>
+                </section>
+                
+                <section id="services" className="services">
+                    <Services/>
+                </section>
+
+                <section id="portfolio" className="portfolio">
+                    <Portfolio/>
+                </section>
+
+                <section id="clients" className="clients">
+                    <Client/>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default Home
